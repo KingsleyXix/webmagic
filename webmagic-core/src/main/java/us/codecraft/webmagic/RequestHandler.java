a5874e056a4e31634fa98ae4c709d741d6ccca50
Christian <christian.kalemba-zayi.etu@univ-lille.fr>
2025-03-22 16:14:00 +0100

    delegate download success and failure handling to RequestHandler

--- webmagic-core/src/main/java/us/codecraft/webmagic/RequestHandler.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/RequestHandler.java
@@ -2,12 +2,20 @@ package us.codecraft.webmagic;
 
 public class RequestHandler {
 
+
+
     public void processRequest(Request request, Spider spider) {
         Page page;
         if (null != request.getDownloader()){
             page = request.getDownloader().download(request, spider);
         } else {
             page = spider.getDownloader().download(request, spider);
+        }
+
+        if (page.isDownloadSuccess()){
+            spider.onDownloadSuccess(request, page);
+        } else {
+            spider.onDownloaderFail(request);
         }
     }
 
