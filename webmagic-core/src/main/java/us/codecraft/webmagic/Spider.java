9034e184b15446765ed38da515db01271c128ad2
Christian <christian.kalemba-zayi.etu@univ-lille.fr>
2026-03-24 15:56:13 +0100

    refactor Spider by extracting request processing logic into RequestHandler (SRP improvement)

--- webmagic-core/src/main/java/us/codecraft/webmagic/Spider.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/Spider.java
@@ -247,7 +247,7 @@ public class Spider implements Runnable, Task {
      * @return this
      */
     public Spider clearPipeline() {
-        pipelines = new ArrayList<Pipeline>();
+        pipelines = new ArrayList<>();
         return this;
     }
 
