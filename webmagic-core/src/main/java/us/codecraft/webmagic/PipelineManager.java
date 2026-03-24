91828208da69ca5e10af0772d0c95a6fe352d3a4
Christian <christian.kalemba-zayi.etu@univ-lille.fr>
2026-03-24 16:29:53 +0100

    add pipelines field and constructor to PipelineManager

--- webmagic-core/src/main/java/us/codecraft/webmagic/PipelineManager.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/PipelineManager.java
@@ -1,4 +1,18 @@
 package us.codecraft.webmagic;
 
+import us.codecraft.webmagic.pipeline.Pipeline;
+
+import java.util.List;
+
+
 public class PipelineManager {
+
+    private List<Pipeline> pipelines;
+
+    public PipelineManager(List<Pipeline> pipelines) {
+        this.pipelines = pipelines;
+    }
+
 }
+
+
