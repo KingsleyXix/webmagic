54b8db21563dc9b5c62e1d79301a98f4c320988f
Christian <christian.kalemba-zayi.etu@univ-lille.fr>
2026-04-05 11:31:56 +0200

    Replace try-finally with try-with-resources in ProxyUtils

--- webmagic-core/src/main/java/us/codecraft/webmagic/utils/ProxyUtils.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/utils/ProxyUtils.java
@@ -20,25 +20,15 @@ public class ProxyUtils {
 	private static final Logger logger = LoggerFactory.getLogger(ProxyUtils.class);
 
 	public static boolean validateProxy(Proxy p) {
-		Socket socket = null;
-		try {
-			socket = new Socket();
-			InetSocketAddress endpointSocketAddr = new InetSocketAddress(p.getHost(), p.getPort());
+		try (Socket socket = new Socket()) {
+			InetSocketAddress endpointSocketAddr =
+					new InetSocketAddress(p.getHost(), p.getPort());
 			socket.connect(endpointSocketAddr, 3000);
 			return true;
 		} catch (IOException e) {
-			logger.warn("FAILRE - CAN not connect!  remote: " + p);
+			logger.warn("FAILURE - Cannot connect! remote: " + p);
 			return false;
-		} finally {
-			if (socket != null) {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					logger.warn("Error occurred while closing socket of validating proxy", e);
-				}
-			}
 		}
-
 	}
 
 }
